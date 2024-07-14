5e338bb30069dfa158091f78ced960bec28f1bd1
Max <scorpiomax90@gmail.com>
2024-07-14 22:48:46 +0300

    change validate func

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       class="max-w-[760px] mx-auto p-5 rounded shadow-md border grid gap-2"
     >
       <!-- <BaseCheckbox v-model:checked="is_checked" /> -->
-      <div :class="{ error: v$.formData.firstName.$errors.length }">
+      <div>
         <BaseInput
           v-model="formData.firstName"
           type="text"
@@ -14,40 +14,76 @@
           required-symbol
         />
         <div
-          class="input-errors"
-          v-for="error of v$.formData.firstName.$errors"
+          class="text-xs text-red-500"
+          v-for="error of v$.firstName.$errors"
           :key="error.$uid"
         >
           <div class="error-msg">{{ error.$message }}</div>
         </div>
       </div>
-      <BaseInput
-        v-model="formData.lastName"
-        type="text"
-        id="last_name"
-        label="Last Name"
-        required-symbol
-      />
-      <BaseInput
-        v-model="formData.email"
-        type="email"
-        id="email"
-        label="Email"
-        required-symbol
-      />
-      <BaseInput
-        v-model="formData.password"
-        type="password"
-        id="password"
-        label="Password"
-        required-symbol
-      />
-      <BaseTagsInput
-        v-model="formData.tags"
-        required-symbol
-        label="Skills"
-        placeholder="Vue.js, css, React, ets"
-      />
+      <div>
+        <BaseInput
+          v-model="formData.lastName"
+          type="text"
+          id="last_name"
+          label="Last Name"
+          required-symbol
+        />
+        <!-- <div
+          class="text-xs text-red-500"
+          v-for="error of v$.formData.lastName.$errors"
+          :key="error.$uid"
+        >
+          <div class="error-msg">{{ error.$message }}</div>
+        </div> -->
+      </div>
+      <div>
+        <BaseInput
+          v-model="formData.email"
+          type="email"
+          id="email"
+          label="Email"
+          required-symbol
+        />
+        <!-- <div
+          class="text-xs text-red-500"
+          v-for="error of v$.formData.email.$errors"
+          :key="error.$uid"
+        >
+          <div class="error-msg">{{ error.$message }}</div>
+        </div> -->
+      </div>
+      <div>
+        <BaseInput
+          v-model="formData.password"
+          type="password"
+          id="password"
+          label="Password"
+          required-symbol
+        />
+        <!-- <div
+          class="text-xs text-red-500"
+          v-for="error of v$.formData.password.$errors"
+          :key="error.$uid"
+        >
+          <div class="error-msg">{{ error.$message }}</div>
+        </div> -->
+      </div>
+      <div>
+        <BaseTagsInput
+          v-model="formData.tags"
+          required-symbol
+          label="Skills"
+          placeholder="Vue.js, css, React, ets"
+        />
+        <!-- <div
+          class="text-xs text-red-500"
+          v-for="error of v$.formData.tags.$errors"
+          :key="error.$uid"
+        >
+          <div class="error-msg">{{ error.$message }}</div>
+        </div> -->
+      </div>
 
       <div class="mt-6 text-end">
         <button
@@ -61,14 +97,22 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 // import BaseCheckbox from "./components/BaseCheckbox.vue";
 import BaseTagsInput from "./components/BaseTagsInput.vue";
 import BaseInput from "./components/BaseInput.vue";
 import { useVuelidate } from "@vuelidate/core";
 import { required, email } from "@vuelidate/validators";
 
-const formData = ref({
+interface Form {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  tags: string[];
+}
+
+const formData = ref<Form>({
   firstName: "",
   lastName: "",
   email: "",
@@ -77,13 +121,11 @@ const formData = ref({
 });
 
 const rules = {
-  formData: {
-    firstName: { required },
-    lastName: { required },
-    email: { required, email },
-    password: { required },
-    tags: { required },
-  },
+  firstName: { required },
+  lastName: { required },
+  email: { required, email },
+  password: { required },
+  tags: { required },
 };
 
 const v$ = useVuelidate(rules, formData);
@@ -94,16 +136,18 @@ async function onSubmit() {
   const result = await v$.value.$validate();
 
   console.log(result);
-  const tags = formData.value.tags.map((i) => i);
+  if (result) {
+    const tags = formData.value.tags.map((i) => i);
 
-  const data = {
-    firstName: formData.value.firstName,
-    lastName: formData.value.lastName,
-    email: formData.value.email,
-    password: formData.value.password,
-    tags: tags,
-  };
+    const data = {
+      firstName: formData.value.firstName,
+      lastName: formData.value.lastName,
+      email: formData.value.email,
+      password: formData.value.password,
+      tags: tags,
+    };
 
-  console.log(data);
+    console.log(data);
+  }
 }
 </script>
